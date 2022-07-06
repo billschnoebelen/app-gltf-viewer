a658cca2a8fe061ad264e670f1acdea8bb7ec6fa
billschnoebelen <bill@circuitcase.com>
2022-07-05 23:45:41 -0700

    Updated configuration.

--- web/js/index.js
+++ web/js/index.js
@@ -249,7 +249,7 @@ const { loadGltf } = initThreeJsElements();
 $elemSelector.addEventListener("change", async (evt) => {
   // Trigger translation by getting /api/gltf
   const selectedOption = evt.target.options[event.target.selectedIndex];
-  if (selectedOption.innerText !== "-- Select an Item --") {
+  if (selectedOption.innerText !== "-- Select an Item index.js --") {
     try {
       document.body.style.cursor = "progress";
       const resp = await fetch(
